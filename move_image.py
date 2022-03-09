0fcf6338103ef66fc2093494161661ea73fe15a5
Đào Công Tú <91873214+daotuai@users.noreply.github.com>
2022-03-09 16:34:45 +0700

    Update move_image.py

--- move_image.py
+++ move_image.py
@@ -1,14 +1,10 @@
 def move_image(image,id,labelID):
 
 	path = CLUSTERING_RESULT_PATH+'/persion'+str(labelID)
-	# os.path.exists() method in Python is used to check whether the specified path exists or not.
-	# os.mkdir() method in Python is used to create a directory named path with the specified numeric mode.
 	if os.path.exists(path) == False:
 		os.mkdir(path)
 
 	filename = str(id) +'.jpg'
-	# Using cv2.imwrite() method
-	# Saving the image
 
 	cv2.imwrite(os.path.join(path , filename), image)
 
